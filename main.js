8e5b2e1b2da946df916c8c172a8cc60fe77dcca3
VoMinhToan <VoMinhToansc.com>
2023-09-23 20:10:37 +0700

    update

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// /** */
+// // var fullName = 'vo minh toan'
+// // console.log(fullName);
+// // console.log(24)
+// // xác nhận
+// // confirm()
+// // Promt
+// // setTimeout( function()  {
+// //     alert('thông báo')
+// // },1000)
+// // setInterval(function () {
+// //     console.log('day l' + Math.random)
+    
+// // },1000)
+// // function showDialog() {
+// //     alert('xin chao')  
+// // }
+// //  call
+// // showDialog()
+// // function write(a,b){
+    
+// //     console.log("123")
+// //     alert
+// //     return a + b
+// // }
+// // var result = write(2,8)
+// // console.log(result)
+// function getUpperCaseName(Case){
+//     return Case.toUpperCaseName
+// }
+
+
+
+// // Expected results:
+// console.log(getUpperCaseName("Nguyen van a")) // "NGUYEN VAN A"
+// console.log(getUpperCaseName("nGuyen vAn C")) // "NGUYEN VAN C"
